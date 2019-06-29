d418ab453e5f3c8ffce2e9a609155cf653d77c32
alexHooper <alexmohack@gmail.com>
2019-06-29 21:16:28 +0530

    add default db settings

--- boilerplate/settings.py
+++ boilerplate/settings.py
@@ -76,6 +76,13 @@ WSGI_APPLICATION = 'boilerplate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
