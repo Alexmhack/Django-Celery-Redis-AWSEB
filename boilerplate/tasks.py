0209846c439be7a6b3263f5f4c4d4afd60133703
alexHooper <alexmohack@gmail.com>
2019-06-29 14:52:06 +0530

    create sample object in tasks

--- boilerplate/tasks.py
+++ boilerplate/tasks.py
@@ -2,8 +2,14 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 
+from samples.models import Sample
+
 @shared_task
 def add(x, y):
-	x = int(x)
-	y = int(y)
-	return (x + y)
+	try:
+		x = int(x)
+		y = int(y)
+		value = (x + y)
+		Sample.objects.create(value=value)
+	except Exception as e:
+		pass
