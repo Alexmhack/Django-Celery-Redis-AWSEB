126ee56391480c4ba1360f69b8d98769de1f4f4f
alexHooper <alexmohack@gmail.com>
2019-06-29 21:16:18 +0530

    set django settings in env

--- boilerplate/celeryapp.py
+++ boilerplate/celeryapp.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
 
+import os
+
 from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boilerplate.settings')
 
 app = Celery('boilerplate', include=['boilerplate.tasks'])
 
