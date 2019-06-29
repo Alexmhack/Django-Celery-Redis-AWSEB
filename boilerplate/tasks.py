c37034cca131c9ee66fe258f7bda7ff48e83396c
alexHooper <alexmohack@gmail.com>
2019-06-29 13:45:09 +0530

    define simple add task

--- boilerplate/tasks.py
+++ boilerplate/tasks.py
@@ -4,4 +4,4 @@ from celery import shared_task
 
 @shared_task
 def add(x, y):
-    return x + y
+    return (x + y)
---
+++ boilerplate/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from .tasks import add
+
+def index_view(request):
+	context = dict()
+	if request.method == "POST":
+		a = request.POST.get('a')
+		b = request.POST.get('b')
+		result = add.apply_async((a, b))
+		context['result'] = result
+	return render(request, 'index.html', context)
